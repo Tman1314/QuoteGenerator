7874419c1dd037d6e33d3df07ae0f5f823b52429
Tman1314 <33041733+Tman1314@users.noreply.github.com>
2023-06-04 06:35:11 -0700

    add api + lamdba + auth + graphql

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,29 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quotes": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "quotes07eca8c1": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "quoteLambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import React, { useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Components
+import { BackgroundImage1, BackgroundImage2, GradientBackgroundCon, FooterCon, FooterLink, RedSpan, QuoteGeneratorCon, QuoteGeneratorInnerCon, QuoteGeneratorTitle, QuoteGeneratorSubTitle, GenerateQuoteButton, GenerateQuoteButtonText } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Assets
+import Clouds1 from '../assets/cloud-and-thunder.png'
+import Clouds2 from '../assets/cloudy-weather.png'
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
+
   return (
     <>
       <Head>
@@ -14,7 +24,55 @@ export default function Home() {
       </Head>
       {/* Background */}
       <GradientBackgroundCon>
-        
+      
+        {/* Quote Generator Modal Pop-up */}
+        {/* <QuoteGeneratorModal
+        /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Quote Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Looking for a nice quote? Generate a quote card with a random quote provided by <FooterLink href="https://zenquotes.io" target="_blank" rel="noopener noreferrer">ZenQuotes API</FooterLink>.
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>
+              Make a Quote
+              </GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+      {/* Background Images */}
+      <BackgroundImage1
+        src={Clouds1}
+        height="300"
+        alt="cloudybackground1"
+       />
+
+      <BackgroundImage2
+        src={Clouds2}
+        height="300"
+        alt="cloudybackground1"
+       />
+
+       {/* Footer */}
+       <FooterCon>
+        <>
+          Quotes Generated: {numberOfQuotes}
+          <br />
+          Developed with <RedSpan>♥</RedSpan> by <FooterLink 
+          href="https://www.linkedin.com/in/tmills-webdev" 
+          target='_blank' rel='noopener noreferrer'>
+          Tanner Mills </FooterLink>
+        </>
+       </FooterCon>
+
       </GradientBackgroundCon>
     </>
   )
